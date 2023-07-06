b2c4c974631a12ae51648bec78ba7ab0d83d5df7
alasmaritza <alas.maritza.j@gmail.com>
2023-07-06 02:35:40 -0700

    initial commit

---
+++ server/db/conn.mjs
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.ATLAS_URI || "";
+const client = new MongoClient(connectionString, { useNewUrlParser: true });
+
+let conn;
+try {
+  conn = await client.connect();
+} catch(e) {
+  console.error(e);
+}
+
+let db = conn.db("");
+
+export default db;
